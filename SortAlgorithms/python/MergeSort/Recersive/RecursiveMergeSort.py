9b46446c457978d70c46e34ffc670ac8e2665b43
amirpourastarabadi <amirpourastarabadi@gmail.com>
2022-04-30 09:25:10 +0430

    complete python RecursiveMergeSort

--- SortAlgorithms/python/MergeSort/Recersive/RecursiveMergeSort.py
+++ SortAlgorithms/python/MergeSort/Recersive/RecursiveMergeSort.py
@@ -3,34 +3,34 @@ class RecursiveMergeSort:
     def sort(self, A):
 
         if len(A) > 1:
-            mid = int (len(A) / 2)
-            A_I = A[0: mid]
-            A_II = A[mid:]
-            self.sort(A_I)
-            self.sort(A_II)
-            return self.__merge(A_I, A_II)
-        else:
-            return A[0]
+            mid = len(A) // 2
+            leftHalf = A[0: mid]
+            rightHalf = A[mid:]
+            self.sort(leftHalf)
+            self.sort(rightHalf)
 
+            i, j, k = 0, 0, 0
 
-    def __merge(self, A, B):
-        i, j = 0, 0
-        C = []
+            while i < len(rightHalf) and j < len(leftHalf):
+                if rightHalf[i] <= leftHalf[j]:
+                    A[k]= rightHalf[i]
+                    i += 1
 
-        while i < len(A) and j < len(B):
-            if A[i] <= B[j]:
-                C.append(A[i])
+                else:
+                    A[k] = leftHalf[j]
+                    j += 1
+
+                k += 1
+
+            while i < len(rightHalf):
+                A[k] = rightHalf[i]
+                k += 1
                 i += 1
-            else:
-                C.append(B[j])
+
+            while j < len(leftHalf):
+                A[k] = leftHalf[j]
+                k += 1
                 j += 1
 
-        while i < len(A):
-            C.append(A[i])
-            i += 1
 
-        while j < len(B):
-            C.append(B[j])
-            j += 1
-
-        return C
+        return A
