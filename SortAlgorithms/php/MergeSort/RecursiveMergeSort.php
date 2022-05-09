78eb49ec8227cd74652d7f4f08f24c1f45cc8e15
amirpourastarabadi <amirpourastarabadi@gmail.com>
2022-05-09 09:41:38 +0430

    refactor php RecursiveMergeSort

--- SortAlgorithms/php/MergeSort/RecursiveMergeSort.php
+++ SortAlgorithms/php/MergeSort/RecursiveMergeSort.php
@@ -6,48 +6,50 @@ class RecursiveMergeSort implements SortArray
     public function sort(array $A): array
     {
 
-        if (count($A) > 1)
-        {
+        if (count($A) > 1) {
             $mid = intVal(count($A) / 2);
             $leftSide = array_slice($A, 0, $mid);
             $rightSide = array_slice($A, $mid, count($A));
             $leftSide = $this->sort($leftSide);
             $rightSide = $this->sort($rightSide);
 
-            $i = $j = $k = 0;
+            return $this->mergeTwoSortedArray($A, $leftSide, $rightSide);
+        }
 
-            while ($i < count($leftSide) && $j < count($rightSide))
-            {
-                if($leftSide[$i] <= $rightSide[$j])
-                {
-                    $A[$k] = $leftSide[$i];
-                    $i += 1;
-                }
-                else{
-                    $A[$k] = $rightSide[$j];
-                    $j += 1;
-                }
-                $k += 1;
-            }
-            while ($i < count($leftSide))
-            {
+        return $A;
+    }
+
+    public function mergeTwoSortedArray(array $main, array $leftSide, array $rightSide): array
+    {
+        $i = $j = $k = 0;
+
+        while ($i < count($leftSide) && $j < count($rightSide)) {
+            if ($leftSide[$i] <= $rightSide[$j]) {
                 $A[$k] = $leftSide[$i];
                 $i += 1;
-                $k += 1;
-            }
-            while ($j < count($rightSide))
-            {
+            } else {
                 $A[$k] = $rightSide[$j];
                 $j += 1;
-                $k += 1;
             }
+            $k += 1;
         }
+        while ($i < count($leftSide)) {
+            $A[$k] = $leftSide[$i];
+            $i += 1;
+            $k += 1;
+        }
+        while ($j < count($rightSide)) {
+            $A[$k] = $rightSide[$j];
+            $j += 1;
+            $k += 1;
+        }
+
 
         return $A;
     }
 
     public function printArray(array $arr)
     {
-        echo "\n[" . implode(',', $arr) . "]\n" ;
+        echo "\n[" . implode(',', $arr) . "]\n";
     }
 }
