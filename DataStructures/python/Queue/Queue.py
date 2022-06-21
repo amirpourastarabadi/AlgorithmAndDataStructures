45f11dd4ad4081313c72129f084ad3e9de9ff27f
amirpourastarabadi <amirpourastarabadi@gmail.com>
2022-06-21 11:21:34 +0430

    imp queue structure for python

---
+++ DataStructures/python/Queue/Queue.py
@@ -0,0 +1,25 @@
+class Queue:
+
+    def __init__(self, size) -> None:
+        self.queue = [None for _ in range(size)]
+        self.size = size
+    
+    def is_full(self):
+        pass
+
+    def is_empty(self):
+        pass
+
+    def enqueue(self, item):
+        pass
+
+    def dequeue(self):
+        pass
+
+    def front(self):
+        pass
+
+    def rear(self):
+        pass
+
+    
