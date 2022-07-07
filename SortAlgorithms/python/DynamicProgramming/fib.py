d4d1aa4fc24ca8fe29d3da65131f63721f53517f
amir <amirpourastarabadi@gmail.com>
2022-07-07 19:37:26 +0430

    recursive version of fibonachi

---
+++ SortAlgorithms/python/DynamicProgramming/fib.py
@@ -0,0 +1,13 @@
+def fib(n):
+    if n == 1 or n == 2 : 
+        return 1
+    return fib(n-1) + fib(n-2)
+
+assert fib(1) == 1
+assert fib(2) == 1
+assert fib(3) == 2
+assert fib(4) == 3
+assert fib(5) == 5
+assert fib(6) == 8
+assert fib(7) == 13
+assert fib(8) == 21
