2d67faa3302f80f1040b81d3c891321fa0479e5d
amir <amirpourastarabadi@gmail.com>
2022-07-08 12:18:35 +0430

    new fib with less memory

--- SortAlgorithms/python/DynamicProgramming/fib.py
+++ SortAlgorithms/python/DynamicProgramming/fib.py
@@ -1,4 +1,6 @@
+counter = []
 def fib(n):
+    counter.append(1)
     if n == 1 or n == 2 : 
         return 1
     return fib(n-1) + fib(n-2)
@@ -11,3 +13,20 @@ assert fib(5) == 5
 assert fib(6) == 8
 assert fib(7) == 13
 assert fib(8) == 21
+
+counter = []
+fib(21)
+print('for fib 21', sum(counter), 'time fib function was called')
+# for fib(21) '21891' times fib function called
+
+
+def fib2(n):
+    data = {}
+    data[1] = 1
+    data[2] = 1
+    for i in range(3, n+1):
+        data[i] = data[i-1] + data[i-2]
+    
+    return data[n]
+
+# fib2 do the same work in O(n)
